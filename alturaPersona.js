fec2b91d15c64958f5c79978d0e319e7d3245831
Negrito <bfonsecanegro@gmail.com>
2024-05-08 19:41:40 -0300

    primer repo

---
+++ alturaPersona.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*let msk: string = "bienvenidos a nuestro sitio web";*/
+/*let paso1:string;
+let paso2:string;
+let paso3:string;
+let paso4:string;
+let paso5:string;
+paso1 = "avanzar y quitar pila"
+paso2 = "avanzar y quitar pila"
+paso3 = "girar 90 grados a la izquierda"
+paso4 = "avanzar y quitar pila"
+paso5 = "avanzar y quitar pila"
+console.log("paso 1:" n\ +paso1 + "paso 2:"+ paso2 +"paso 3:" +paso3+ "paso 4:" +paso4 +"paso 5:" +paso5);*/
+var readlineSync = require("readline-sync");
+var alturaPersona = readlineSync.question("Ingrese la altura: ");
+console.log("La persona mide: " + alturaPersona + " metros");
